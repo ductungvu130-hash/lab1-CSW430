ddf9d7d9d014902719fc45077558dfda0d06fa8c
Vũ Đức Tùng <ductungvu130@gmail.com>
2026-07-03 08:39:50 +0700

    Finish

---
+++ Q3Expo1/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import { Text,Button, View } from 'react-native';
+
+export default () =>{
+  const [pressCount, setPressCount] = useState(0);
+
+  return(
+    <View style = {{alignItems : "center", marginTop :200}}>
+        <Text>You've pressed the button: {pressCount} time (s)</Text>
+        <Button title = "Press me" onPress={() => setPressCount(pressCount + 1)}></Button>
+    </View>
+  );
+};
+
